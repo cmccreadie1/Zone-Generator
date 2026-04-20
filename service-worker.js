fd0e1c66ed4543bdaadfc7ae13f1633a8cd320dd
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-04-20 19:12:57 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'fishing-comp-v7.5.0';
+const ASSETS = ['./', './index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+});
