a62a27a1a53aeedbb85f65ad93c91acf27d99d32
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-05-03 08:13:55 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,43 @@
-const CACHE_NAME = 'zonedraw-v8.6.0-hd-v1';
+const CACHE_NAME = 'zonedraw-v8.7.0-protocol-v1';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
     './manifest.json',
-    'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
-    'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
+    './icon.svg',
+    'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
 ];
 
+// Install Event: Caching the new v8.7.0 assets
 self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
     self.skipWaiting();
 });
 
+// Activate Event: Purging old versions to save space on your laptop
 self.addEventListener('activate', (event) => {
-    event.waitUntil(caches.keys().then((names) => Promise.all(names.map((n) => { 
-        if(n !== CACHE_NAME) return caches.delete(n); 
-    }))));
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
     self.clients.claim();
 });
 
+// Fetch Event: Ensuring offline reliability on the bankside
 self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
